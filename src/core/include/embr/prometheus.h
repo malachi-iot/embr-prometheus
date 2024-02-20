63dbd2808a50bfe4b9399a161c9f76123da12610
Malachi Burke <malachi.burke@gmail.com>
2024-02-19 22:18:58 -0800

    Coming along
    A ways to go.  Starting to resemble prometheus though

--- src/core/include/embr/prometheus.h
+++ src/core/include/embr/prometheus.h
@@ -8,6 +8,132 @@ namespace prometheus {
 
 void synthetic();
 
+struct metric_tag {};
+
+// buckets MUST appear in ascending order
+template <typename T, T... buckets>
+class Histogram : metric_tag
+{
+#if UNIT_TESTING
+public:
+#endif
+    static constexpr unsigned bucket_count = sizeof...(buckets);
+
+    T buckets_[bucket_count] {};
+
+public:
+    void observe_idx(unsigned bucket_idx)
+    {
+        ++buckets_[bucket_idx];
+    }
+
+    bool observe(T value)
+    {
+        // Lots of help from
+        // https://www.foonathan.net/2020/05/fold-tricks/
+
+        unsigned i = 0;
+        unsigned bucket;
+        bool valid = (((i++ < bucket_count && value < buckets) && (bucket = i, true)) || ...);
+
+        if(valid)   observe_idx(bucket - 1);
+
+        return valid;
+    }
+};
+
+
+template <class T>
+class Counter : metric_tag
+{
+    T value_;
+
+public:
+    void inc()
+    {
+        ++value_;
+    }
+};
+
+
+template <class T>
+class Gauge : metric_tag
+{
+    T value_ {};
+
+public:
+    void add(T value)
+    {
+        value_ += value;
+    }
+
+    constexpr const T& value() const { return value_; }
+};
+
+class Summary : metric_tag
+{
+
+};
+
+// Guidance from
+// https://sysdig.com/blog/prometheus-metrics/
+// https://prometheus.io/docs/instrumenting/writing_exporters/
+
+// Gonna need to be smarter and more complex due to the way buckets work
+// (i.e. every bucket entry probably wants custom labels too)
+
+template <class Stream>
+class OutAssist
+{
+    Stream& out_;
+    int labels_ = 0;
+    const char* name_;
+
+    void finalize_label()
+    {
+        if(labels_ > 0) out_ << '}';
+    }
+
+public:
+    OutAssist(Stream& out, const char* metric_name) : out_{out},
+        name_{metric_name}
+    {
+        // FIX: Need to do this as base name and append bucket, count, sum, total, etc
+        out_ << metric_name;
+    }
+
+    void label(const char* label, const char* value)
+    {
+        if(labels_ == 0)
+            out_ << '{';
+        else
+            out_ << ", ";
+
+        out_ << label << "=\"";
+        out_ << value;
+        out_ << '"';
+
+        ++labels_;
+    }
+
+    template <class T>
+    void metric(const Gauge<T>& value)
+    {
+        finalize_label();
+
+        out_ << ' ' << value.value();
+    }
+
+    template <class T, T... buckets>
+    void metric(const Histogram<T, buckets...>& value)
+    {
+        finalize_label();
+
+        //out_ << ' ' << value.value();
+    }
+};
+
+
 }
 
 }
--- test/catch/main.cpp
+++ test/catch/main.cpp
@@ -1,8 +1,32 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include <embr/prometheus.h>
+#include <estd/sstream.h>
+
+using namespace embr::prometheus;
 
 TEST_CASE("TEST")
 {
+    estd::layer1::ostringstream<128> out;
+    const auto& str = out.rdbuf()->str();
+    Gauge<int> g;
+    Histogram<int, 0, 10, 20, 30> h;
+    OutAssist<decltype(out)> oa(out, "metric1");
 
+    h.observe(15);
+    h.observe(21);
+    h.observe(25);
+
+    REQUIRE(h.buckets_[0] == 0);
+    REQUIRE(h.buckets_[2] == 1);
+    REQUIRE(h.buckets_[3] == 2);
+
+    REQUIRE(h.observe(35) == false);
+
+    g.add(23);
+
+    oa.label("instance", "1");
+    oa.metric(g);
+
+    REQUIRE(str == "metric1{instance=\"1\"} 23");
 }
